032708ff8ded3d26dc5bf1670339603decf2efa4
luiggy macias <ferrinluiggy@gmail.com>
2023-02-10 15:06:50 -0500

    metodos http

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('api/v1/task')
-export class TaskController {}
+export class TaskController {
+  @Post()
+  methodPost(@Req() req: Request) {
+    return `method: ${req.method}`
+    // return res.status(200).json({ message: 'method: ' + req.method });
+  }
+
+  @Get('done')
+  methodGet(@Req() req: Request, @Res() res: Response) {
+    return res.status(200).json({ message: 'method: ' + req.method });
+  }
+
+  @Put('done')
+  methodPut(@Req() req: Request, @Res() res: Response) {
+    return res.status(200).json({ message: 'method: ' + req.method });
+  }
+
+  @Delete('done')
+  methodDelete(@Req() req: Request, @Res() res: Response) {
+    return res.status(200).json({ message: 'method: ' + req.method });
+  }
+
+  // @Post
+} 
