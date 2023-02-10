24ad5f4911ab4d420fe23e858164560281e80023
luiggy macias <ferrinluiggy@gmail.com>
2023-02-10 15:17:59 -0500

    params y query

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Delete, Get, Post, Put, Req, Res } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Controller('api/v1/task')
 export class TaskController {
   @Post()
   methodPost(@Req() req: Request) {
-    return `method: ${req.method}`
+    return `method: ${req.method}`;
     // return res.status(200).json({ message: 'method: ' + req.method });
   }
 
@@ -24,5 +34,20 @@ export class TaskController {
     return res.status(200).json({ message: 'method: ' + req.method });
   }
 
-  // @Post
-} 
+  @Post(':id/description/:description/isDone/:isDone')
+  // methodPostId(@Param('id') id: string, @Req() req: Request, @Res() res: Response) {
+  //   return res.status(200).json({ message: 'method: ' + req.method, id: id });
+  // }
+  methodPostId(
+    @Param() param: { id: string; description: string; isDone: string },
+  ) {
+    const { id } = param;
+    return { id, param };
+  }
+
+  @Get()
+  methodGetQuery(@Query() query: { description: string; isDone: string }) {
+    const { description, isDone } = query;
+    return { description, isDone };
+  }
+}
